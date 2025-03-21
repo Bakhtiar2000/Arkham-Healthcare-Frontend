23ab0f7918ecc773ae3d5d59bc980b7693afea50
Bakhtiar <bakhtiarfahim360@gmail.com>
2025-03-21 10:27:14 +0600

    added role based dashboard

---
+++ src/app/(withDashboardLayout)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DashboardHomePage = () => {
+  return (
+    <div>
+      <h2>Dashboard Home Page</h2>
+    </div>
+  );
+};
+
+export default DashboardHomePage;
